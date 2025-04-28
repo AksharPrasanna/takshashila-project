91014634a7ab3567ded602253087e6f317f25457
Quarto GHA Workflow Runner <quarto-github-actions-publish@example.com>
2025-04-28 15:09:57 +0000

    Built site for gh-pages

--- utility/sidenote.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const titleBlock = document.getElementById("quarto-margin-sidebar");
-    const contentBlock = document.getElementById("sidenote");
-
-    if (titleBlock && contentBlock) {
-        titleBlock.append(contentBlock);
-    }
-});
